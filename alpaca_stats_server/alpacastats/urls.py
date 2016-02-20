970093ab26b91e79e7f845de1af6b40c8390244b
Oliver Stenbom <oliver@stenbom.eu>
2016-02-20 13:43:41 +0000

    Added api urls

--- alpaca_stats_server/alpacastats/urls.py
+++ alpaca_stats_server/alpacastats/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 
-from . import views
+from views import apis, public_views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', public_views.home, name='index'),
+    url(r'^api/events$', apis.events, name='events_api'),
+    url(r'^api/pool$', apis.pool, name='pool_api'),
+    url(r'^api/vote_track$', apis.vote_track, name='vote_track_api'),
+    url(r'^api/vote_request$', apis.vote_request, name='vote_request_api'),
+    url(r'^api/movement$', apis.movement, name='movement_api'),
+    url(r'^api/vote$', apis.vote, name='vote_api'),
+    url(r'^api/add_request$', apis.add_request, name='requests_api'),
 ]
--- alpaca_stats_server/alpacastats/views/public_views.py
+++ alpaca_stats_server/alpacastats/views/public_views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
     return HttpResponse("Hello, world. You're at the index.")
