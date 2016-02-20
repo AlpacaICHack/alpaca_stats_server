acda6ac316463c4316c6c080ffe3222a83dc5bf1
Oliver Stenbom <oliver@stenbom.eu>
2016-02-20 13:53:15 +0000

    api views spacing

--- alpaca_stats_server/alpacastats/views/apis.py
+++ alpaca_stats_server/alpacastats/views/apis.py
@@ -1,23 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def events(request):
     pass
+
 
 def pool(request):
     pass
 
+
 def vote_track(request):
     pass
+
 
 def vote_request(request):
     pass
 
+
 def movement(request):
     pass
 
+
 def vote(request):
     pass
+
 
 def add_request(request):
     pass
