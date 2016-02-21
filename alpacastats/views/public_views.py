bc77478ab96dfce7f6cc3cb50a9f807f5df8a6e7
Oliver Stenbom <oliver@stenbom.eu>
2016-02-21 00:52:32 +0000

    Home page complete

--- alpacastats/views/public_views.py
+++ alpacastats/views/public_views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from ..models import Event, Track, Movement, Vote
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'alpacastats/pools.html')
+    events = Event.objects.all().order_by('-date')
+
+    currenttracks = []
+    for e in events:
+        if len(Track.objects.filter(event__pk=e.pk).filter(active_track=True)) > 0:
+            currenttrack = Track.objects.filter(event__pk=e.pk).filter(active_track=True)[0]
+        else:
+            currenttrack = None
+        currenttracks.append(currenttrack)
+
+    context = {'events': zip(events, currenttracks)}
+    return render(request, 'alpacastats/home.html', context)
